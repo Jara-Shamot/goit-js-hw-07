54620ccb46d895248d7b26b4b1d1cb8af2aa4987
Jara-Shamot <141449562+Jara-Shamot@users.noreply.github.com>
2024-01-15 12:16:57 +0100

    edit css

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-js-hw-07
+
+#Tetiana https://github.com/Jara-Shamot/goit-js-hw-07
---
+++ js/task-03.js
@@ -0,0 +1,7 @@
+const inputElement = document.querySelector("#name-input");
+const outputElement = document.querySelector("#name-output");
+
+inputElement.addEventListener("input", (event) => {
+  let textContent = event.currentTarget.value.trim();
+  outputElement.textContent = textContent ? textContent : "Anonymous";
+});
